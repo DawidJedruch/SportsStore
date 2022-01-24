797a76e4c66f8fa2265b80420d281edbe364cbca
Dawid Jedruch <dawidszymonjedruch@gmail.com>
2022-01-24 12:05:03 +0100

    add NinjectDependencyResolver

---
+++ SportsStore.WebUI/Infrastrucutre/NinjectDependencyResolver.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using Ninject;
+
+namespace SportsStore.WebUI.Infrastrucutre
+{
+    public class NinjectDependencyResolver : IDependencyResolver 
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            //put additional bindings here
+        }
+    }
+}
