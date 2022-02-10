1cb135039f3f2416f15b80c5b44ddcffde7fbb72
Dawid Jedruch <dawidszymonjedruch@gmail.com>
2022-02-10 13:54:22 +0100

    Adding pagination

---
+++ SportsStore.WebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SportsStore.WebUI.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
