1f4afa3fb37e0ac4f1222d9dc0fc342040841941
Dawid Jedruch <dawidszymonjedruch@gmail.com>
2022-02-17 11:48:52 +0100

    Building a shopping cart

---
+++ SportsStore.WebUI/Models/CartIndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SportsStore.WebUI.Models
+{
+    public class CartIndexViewModel
+    {
+        public Cart Cart { get; set; }
+
+        public string ReturnUrl { get; set; }   
+    }
+}
