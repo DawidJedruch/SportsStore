9e264a019ec20a64f909f7550edf6fcb48b34e74
Dawid Jedruch <dawidszymonjedruch@gmail.com>
2022-03-10 16:21:00 +0100

    Handling POST requests in edit view

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -12,10 +12,19 @@ namespace SportsStore.Domain.Entities
     {
         [HiddenInput(DisplayValue  = false)]
         public int ProductID { get; set; }
+
+        [Required(ErrorMessage ="Proszę podać nazwę produktu.")]
         public string Name { get; set; }
+
         [DataType(DataType.MultilineText), Display(Name = "Opis")]
+        [Required(ErrorMessage ="Proszę podać opis.")]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage ="Proszę podać dodatnią cenę.")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage ="Prosze określić kategorię.")]
         public string Category { get; set; }
     }
 }
