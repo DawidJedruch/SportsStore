56a5fe66dc102f1a0f8eda50b0352245201fc68a
Dawid Jedruch <dawidszymonjedruch@gmail.com>
2022-03-15 17:49:05 +0100

    simple security policy part1

---
+++ SportsStore.WebUI/Infrastructure/Abstract/IAuthProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SportsStore.WebUI.Infrastructure.Abstract
+{
+    internal interface IAuthProvider
+    {
+        bool Authenticate(string username, string password);
+    }
+}
---
+++ SportsStore.WebUI/Infrastructure/Concrete/FormsAuthProvider.cs
@@ -0,0 +1,23 @@
+﻿using SportsStore.WebUI.Infrastructure.Abstract;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web.Security;
+
+namespace SportsStore.WebUI.Infrastructure.Concrete
+{
+    internal class FormsAuthProvider : IAuthProvider
+    {
+        public bool Authenticate(string username, string password)
+        {
+            bool result = FormsAuthentication.Authenticate(username, password);
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+            }
+            return result;
+        }
+    }
+}
